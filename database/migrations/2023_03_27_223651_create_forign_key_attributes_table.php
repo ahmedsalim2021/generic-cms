32f74eb5528c6c0baa87e71c7fd1ad4342383773
Ahmed Salim <ahmed.salim@adamtravel.net>
2023-03-28 00:37:33 +0200

    install laravel and make models and migrations

---
+++ database/migrations/2023_03_27_223651_create_forign_key_attributes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forign_key_attributes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forign_key_attributes');
+    }
+};
